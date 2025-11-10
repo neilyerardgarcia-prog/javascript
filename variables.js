9e99059e4ecceea3f46d0b201aaf98122796412b
neilg <neilyerard.garcia@institutvidreres.cat>
2025-11-10 11:02:56 +0100

    Primera tarea de java script

---
+++ variables.js
@@ -0,0 +1,25 @@
+console.log("Nom: Neil Yerard")
+console.log("Cognoms: Garcia Colque")
+console.log("Data neixement: 2006")
+
+let nombre = "Neil";
+let edad = 19;
+let estudiante = true;
+
+console.log(nombre, edad, estudiante);
+
+/*
+console.log(x);
+var x = 10;
+console.log(y);
+let y = 10;
+*/
+
+let dada = 123;
+console.log(typeof dada);
+dada1 = "Hola";
+console.log(typeof dada1);
+
+const cambio = 123;
+cambio = 122;
+console.log(cambio)
